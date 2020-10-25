1fddc186c5fa662f7392841e2d817e20a9156401
Jinnapat Yana <jaja_yana@hotmail.com>
2020-10-26 02:13:14 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.end('Hello world')
+});
+
+app.listen(5000, () => {
+    console.log("listen on port 5000");
+});
