2f20eafa722878fe7c24c96995f1cdfc29df851d
TIWYY_ASUS <tiwyy13@gmail.com>
2020-10-26 15:32:47 +0700

    base temp

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MenuList from './first_page'
 
+const el = <MenuList/>;
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {el}
   </React.StrictMode>,
   document.getElementById('root')
 );
