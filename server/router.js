a40b0432fb420e7b7bfecaa3e0a0ff03c1a70180
TIWYY_ASUS <tiwyy13@gmail.com>
2020-10-27 16:35:49 +0700

    fetch the data correctly

--- server/router.js
+++ server/router.js
@@ -1,4 +1,4 @@
-const express = require("express");
+var express = require("express");
 var app = express();
 // listen at port 5000
 var port = process.env.PORT || 5000;
@@ -8,6 +8,9 @@ app.listen(port, () => console.log("Backend server live on " + port));
 
 // GET method from /get_menu
 // Scheama fetched from MENU Database
+// MySQL 8.0 Server/thickinc_gang/MENU/
+// https://www.db4free.net/phpMyAdmin/tbl_sql.php?db=thickinc_gang&table=MENU
+
 
 // {
 //     ...
@@ -23,28 +26,47 @@ app.listen(port, () => console.log("Backend server live on " + port));
 //         ]
 //     }
 // }
-app.get("/get_menu", (req, res) => {
-    menu_db = [];
 
-    var mysql = require('mysql');
+var mysql = require("mysql");
+var pool = mysql.createPool({
+    canRetry: false,
+    connectionLimit : 100,
+    host     : 'db4free.net',
+    port     :  3306,
+    user     : 'da_think',
+    password : '12345678',
+    database : 'thickinc_gang',
+});
+app.get("/get_menu", (req, res) => {
+    menu_db = ['init'];
+    var sql = 'SHOW tables;';
+
+    // async function queryIt (pool, sql){
+    //     try{
+    //         console.log("Just start queryIt");
+    //         var connection = await pool.getConnection();
+    //         var result = await connection.query(sql);
+    //         console.log("Result fetched");
+    //         console.log(result);
+    //         return result;
+    //     } catch (e){
+    //         console.log(e);
+    //     }
+    // }
+    // var sql = 'SHOW tables;';
+    // menu_db = queryIt(pool, sql);
+    // console.log("End");
     
-    var con = mysql.createConnection({
-        host: "db4free.net",
-        user: "da_think",
-        password: "12345678",
-        dbname: "thinkinc_gang"
-    });
-  
-    con.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected to menu DB");
-        var sql = 'SELECT * FROM menu';
-        con.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("Result fetched");
-            menu_db = result;
+    pool.getConnection((err, connection) => {
+        if(err) throw err;
+        console.log('connected as id ' + connection.threadId);
+        connection.query(sql, (err, rows) => {
+            if(err) throw err;
+            console.log('The data from users table are: \n', rows);
+            res.send({ all_menu: rows});
         });
     });
-
-    res.send({ all_menu: menu_db});
 });
+
+
+// app.post('/')
