011c469a66da1443d5ff39cf7daf2353d5ad5183
TIWYY_ASUS <tiwyy13@gmail.com>
2020-11-01 21:32:00 +0700

    add server

--- server/gateway.js
+++ server/gateway.js
@@ -2,6 +2,12 @@
 // var router = express.Router()
 var express = require("express");
 var app = express();
+var port = process.env.PORT || 5000;
+
+
+app.listen(port, function() {
+   console.log('listening on port: 5000');
+});
 
 var userpath = require('./userpath')
 var shoppath = require('./shoppath')
